c0cfb04cd786f11bf30a5716c61563fef873a1b9
Topcatyo <topkatyo@yahoo.com>
2024-10-20 08:41:23 -0400

    1. Started bookbot assignment before work

---
+++ main.py
@@ -0,0 +1,29 @@
+def main():
+    # user chooses book from directory
+    input = "frankenstein"
+    chosen_book = f"books/{input}.txt"
+
+    # book  text is printed
+    text = get_book(chosen_book)
+
+    #get word count
+    word_count = get_word_count(text)
+    
+    #print assignment junk
+    #print(text)
+    print(word_count)
+
+
+def get_book(chosen_book):
+    with open(chosen_book) as b:
+        book = b.read()
+        return book
+
+def get_word_count(book):
+    text = book
+    words = text.split()
+    return len(words)
+
+
+
+main()
